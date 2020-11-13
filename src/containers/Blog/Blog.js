c71b9c0d725ad88bde124f2bbb822f751a301976
Jake <jacob.w.moreno@gmail.com>
2020-11-13 10:51:15 -0700

    Styled the links with React Router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -13,19 +13,25 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            {/* "active" is the default anyway */}
+                            <li><NavLink activeClassName="active" to="/" exact
+                            activeStyle={{
+                              color: '#fa923f',
+                              textDecoration: 'underline'
+                            }}>Home</NavLink></li>
+                            <li><NavLink to={{
                               // This is a dynamically-built 'relative path.' If you ever want to build a link that appends the path to the end of the current path, use this approach! Otherwise, it always generates an absolute path.
-                                pathname: this.props.match + '/new-post',
+                                // pathname: this.props.match + '/new-post',
+                                pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-                <Route path="/" exact component={Posts} />
+                <Route exact path="/" component={Posts} />
                 <Route path="/new-post" component={NewPost} />
             </div>
         );
