a0fab3d576071c315ba6e3961cf073f490b0ab0f
Jake <jacob.w.moreno@gmail.com>
2020-11-13 11:56:52 -0700

    Now users navigate to the individual posts using props.history.push

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -30,21 +29,25 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
+      this.props.history.push({pathname: '/posts/' + id});
+      // this.props.history.push('/posts/' + id);
     }
 
     render () {
         let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
-                return ( <Link to={'/posts/' + post.id} key={post.id}>
+                return (
+                // <Link to={'/posts/' + post.id} key={post.id}>
                   <Post
+                    key={post.id}
                     title={post.title}
                     author={post.author}
                     // {...this.props}
                     clicked={() => this.postSelectedHandler(post.id)}
                   />
-                </Link> );
+                // </Link>
+                );
             });
         }
 
